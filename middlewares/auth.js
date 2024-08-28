cf42fec2e3ca272a2c9c645b6f6288677553daaf
Sahajaddin4 <sahajaddinsk786@gmail.com>
2024-08-28 15:20:06 +0530

    Auth , students middlewares done

---
+++ middlewares/auth.js
@@ -0,0 +1,65 @@
+//import 
+const jwt=require('jsonwebtoken');
+require('dotenv').config();
+
+//middlewares for authentication 
+exports.auth=(req,res,next)=>{
+      
+  try {
+    const {token}=req.body;
+
+    if(!token)
+    {
+        return res.status(401).json({
+            success:false,
+            message:"Token not found!"
+        });
+    }
+
+    // 
+
+    try{
+        let payload=jwt.verify(token,process.env.JWT_SECRET);
+        
+        //req ke body mein role ko bhejo
+        req.user=payload;
+        next();
+    }catch(error){
+        return res.status(401).json({
+            success:false,
+            message:"Token invalid!"
+        });
+    }
+  } catch (error) {
+    return res.status(500).json({
+        success:false,
+        message:"error in middlewares"
+    });
+  }
+};
+
+
+//Student authentication
+exports.isStudent=(req,res,next)=>{
+      try {
+        const {role}=req.user.role;
+
+      if(role!="Student"){
+        return res.status(401).json({
+            success:false,
+            message:"Protected for students!"
+        });
+        
+      }
+
+      //else if role===student
+      next();
+
+
+      } catch (error) {
+        return res.status(500).json({
+        success:false,
+        message:"error in student middlewares"
+    });
+      }
+}
--- routes/userAuthRoutes.js
+++ routes/userAuthRoutes.js
@@ -1,7 +1,18 @@
 const router=require('express').Router();
 const {signup, userLogin}=require('../controller/userAuthController');
+const { auth,isStudent } = require('../middlewares/auth');
 
 router.post('/signup',signup);
-router.post('/login', userLogin)
+router.post('/login', userLogin);
+
+
+
+//middlewares 
+router.get('/student',auth,isStudent,(req,res)=>{
+     res.status(200).json({
+        success:false,
+        message:"Welcome to  student dashboard.."
+    });
+})
 ///export router
 module.exports=router
